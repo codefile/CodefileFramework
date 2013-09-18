9bda1d8247d5cc694537b83b5101b36b7ac85288
Codefile <fabiorocha@codefile.com.br>
2013-09-17 22:21:01 -0300

    Adicionando primeira versão do Repositório Genérico

---
+++ GenericRepository/GenericRepository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace GenericRepository
+{
+    public class GenericRepository<TContext> : Interfaces.IGerericRepository
+        where TContext : DbContext, new ()
+    {
+
+        public TContext Context { get; private set; }
+
+        public GenericRepository()
+        {
+            Context = new TContext();
+        }
+
+        public T Insert<T>(T item, bool saveNow) where T : class
+        {
+            Context.Entry(item).State = System.Data.EntityState.Added;
+            if (saveNow)
+                Context.SaveChanges();
+
+            return item;
+        }
+
+        public T Update<T>(T item, bool saveNow) where T : class
+        {
+            Context.Entry(item).State = System.Data.EntityState.Modified;
+            if (saveNow)
+                Context.SaveChanges();
+
+            return item;
+        }
+
+        public T Delete<T>(T item, bool saveNow) where T : class
+        {
+            Context.Entry(item).State = System.Data.EntityState.Deleted;
+            if (saveNow)
+                Context.SaveChanges();
+
+            return item;
+        }
+
+        public void Dispose()
+        {
+            if (Context != null)
+                Context.Dispose();
+        }
+
+        public int Save()
+        {
+            return Context.SaveChanges();
+        }
+
+        public IEnumerable<T> Select<T>()
+            where T :class
+        {
+            return Context.Set<T>();
+        }
+    }
+}
---
+++ GenericRepository/Interfaces/IGerericRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GenericRepository.Interfaces
+{
+    /// <summary>
+    /// Interface de contrato para um repositório genérico.
+    /// </summary>
+    public interface IGerericRepository : IDisposable
+    {
+        T Insert<T>(T item, bool saveNow)
+            where T : class;
+
+        T Update<T>(T item, bool saveNow)
+            where T : class;
+
+        T Delete<T>(T item, bool saveNow)
+            where T : class;
+
+        int Save();
+
+        IEnumerable<T> Select<T>()
+            where T : class;
+    }
+}
